aefaf77413ed0a79cb40773d7f7859718a14ecc5
HichamOujari <67854522+HichamOujari@users.noreply.github.com>
2021-12-08 00:33:29 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from my repo - Jenkins tp");
+		System.out.println("Hello from my repo - Jenkins tp 2");
 	}
 
 }
